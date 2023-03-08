1e1479e9dd63069ff59f0fde7a22ca51de92c1c2
Tony Hunter <thunter1987@yahoo.com>
2023-03-08 17:49:03 -0600

    "All Tests Passed"

--- index.js
+++ index.js
@@ -1,1 +1,29 @@
 // Write your solution here!
+const cats = ["Milo", "Otis", "Garfield"];
+
+function destructivelyAppendCat(name) {
+    cats.push(name);
+}
+function destructivelyPrependCat(name) {
+    cats.unshift(name);
+}
+function destructivelyRemoveLastCat() {
+    cats.pop();
+}
+function destructivelyRemoveFirstCat() {
+    cats.shift();
+}
+function appendCat(name) {
+    const copyOfCats = [...cats, name];
+    return copyOfCats;
+}
+function prependCat(name) {
+    const copyOfCats = [name, ...cats];
+    return copyOfCats;
+}
+function removeLastCat() {
+    return cats.slice(0, cats.length - 1);
+}
+function removeFirstCat() {
+    return cats.slice(1);
+}
